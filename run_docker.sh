da2689a794c77752c288c164884f35abd68b8518
omer aiman <ew18@protonmail.com>
2020-02-08 12:00:57 +0300

    fixing upload_docker file

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 # Step 1:
 # Build image and add a descriptive tag
 
-docker build -d flaskapp:2.0 .
+docker build -t flaskapp:2.0 .
 
 # Step 2: 
 # List docker images
@@ -14,4 +14,4 @@ docker ps
 # Step 3: 
 # Run flask app
 
-docker run -p 8081:80 -t flaskapp:2.0
+docker run -d -p 8081:80 -t flaskapp:2.0
