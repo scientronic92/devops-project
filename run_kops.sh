2a9ee0d350ed9c7325ebbd843fd82bb531f8059e
omer aiman <ew18@protonmail.com>
2020-02-08 17:22:34 +0300

    [fix]:kops create secret key

--- run_kops.sh
+++ run_kops.sh
@@ -1,1 +1,2 @@
+kops create secret --name kubernetes.maincloud.space sshpublickey admin -i ~/.ssh/id_rsa.pub
 kops create cluster --name=kubernetes.maincloud.space --state=s3://mykops-state-cluster --zones=us-west-1c --node-count=2 --node-size=t2.micro --dns-zone=kubernetes.maincloud.space
