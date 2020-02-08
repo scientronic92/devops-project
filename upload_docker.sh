eba0da99559660a62d155c89a10beb395a4176f3
omer aiman <ew18@protonmail.com>
2020-02-08 11:52:08 +0300

    fixing upload_docker file

--- upload_docker.sh
+++ upload_docker.sh
@@ -12,9 +12,9 @@ dockerpath=scientronic92/flaskapp
 # Authenticate & tag
 
 echo "Docker ID and Image: $dockerpath"
-docker tag flaskapp:2.0 scientronic92/flaskapp
+docker tag flaskapp:2.0 scientronic92/flaskapp:2
 
 # Step 3:
 # Push image to a docker repository
 
-docker push flaskapp:2.0 scientronic92/flaskapp
+docker push flaskapp:2.0 scientronic92/flaskapp:2
