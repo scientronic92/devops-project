b5d54064f8d815999ac77b55fa8c39f09905aed4
omer aiman <ew18@protonmail.com>
2020-02-08 22:24:41 +0300

    [fix]:kops

--- run_kops.sh
+++ run_kops.sh
@@ -1,2 +1,2 @@
 kops create cluster --name=kubernetes.maincloud.space --state=s3://mykops-state-cluster --zones=us-west-1c --node-count=2 --node-size=t2.micro --dns-zone=kubernetes.maincloud.space
-kops update cluster kubernetes.maincloud.space
+kops update cluster kubernetes.maincloud.space --state=s3://mykops-state-cluster
